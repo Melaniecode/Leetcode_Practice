4ec68372b36a28823b36cf9d9ef4c3c70bc6a71f
Melanie Chiu <76036950+Melaniecode@users.noreply.github.com>
2024-07-11 16:36:21 +0800

    Time: 35 ms (10.38%), Space: 29.8 MB (16.96%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,21 +1,34 @@
-// bubble sort
 class Solution {
-public:
+public :
     vector<int> sortedSquares(vector<int>& nums) {
         int n = nums.size();
-        
         for(int i = 0; i < n; i++) {
-            nums[i] = pow(nums[i], 2);
+            nums[i] *= nums[i];
         }
-        for(int i = 0; i < n - 1; i++) {
-            for(int j = 0; j < n - 1 - i; j++) {
-                if(nums[j] > nums[j + 1]) {
-                    int temp = nums[j];
-                    nums[j] = nums[j + 1];
-                    nums[j + 1] = temp;
-                }
-            }
-        }
+        
+        sort(nums.begin(), nums.end());
         return nums;
     }
 };
+
+// bubble sort
+// class Solution {
+// public:
+//     vector<int> sortedSquares(vector<int>& nums) {
+//         int n = nums.size();
+
+//         for(int i = 0; i < n; i++) {
+//             nums[i] = pow(nums[i], 2);
+//         }
+//         for(int i = 0; i < n - 1; i++) {
+//             for(int j = 0; j < n - 1 - i; j++) {
+//                 if(nums[j] > nums[j + 1]) {
+//                     int temp = nums[j];
+//                     nums[j] = nums[j + 1];
+//                     nums[j + 1] = temp;
+//                 }
+//             }
+//         }
+//         return nums;
+//     }
+// };
