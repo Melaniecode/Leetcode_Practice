dc13417c760ba7d80cfc58d8e28d4e0515efdb8d
Melanie Chiu <76036950+Melaniecode@users.noreply.github.com>
2024-07-11 15:54:44 +0800

    Time: 5 ms (27.67%), Space: 10.5 MB (8.36%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,16 +1,32 @@
+// fast：找新array(不含val)
+// slow：指向更新的位置
+
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int size = nums.size();
-        for (int i = 0; i < size; i++) {
-            if (nums[i] == val) { 
-                for (int j = i + 1; j < size; j++) {
-                    nums[j - 1] = nums[j]; // 全都往前一格
-                }
-                i--; 
-                size--;
+        int slow = 0;
+        for(int fast=0; fast < nums.size(); fast++) {
+            if(val != nums[fast]) {
+                nums[slow] = nums[fast];
+                slow++; // nums[slow++] = nums[fast];
             }
         }
-        return size;
+        return slow;
     }
 };
+// class Solution {
+// public:
+//     int removeElement(vector<int>& nums, int val) {
+//         int size = nums.size();
+//         for (int i=0; i<size; i++) {
+//             if (nums[i] == val) { 
+//                 for (int j = i+1; j < size; j++) {
+//                     nums[j-1] = nums[j]; // 全都前移一格
+//                 }
+//                 i--; 
+//                 size--;
+//             }
+//         }
+//         return size;
+//     }
+// };
