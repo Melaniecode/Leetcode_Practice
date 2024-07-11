0b6efc6eff0bf178c3428c588667cc92a3753b6e
Melanie Chiu <76036950+Melaniecode@users.noreply.github.com>
2024-07-11 14:02:49 +0800

    Time: 26 ms (62.89%), Space: 29.9 MB (67.05%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -2,18 +2,13 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         
-        int left = 0;
-        int right = nums.size()-1;        
-        
-        while(left <= right) {
-            int middle = (right+left)/2;
-            if(nums[middle] == target)
-                return middle;
-            else if(target < nums[middle])
-                right--;
-            else
-                left++;
+        int index = -1;
+        for(int i=0; i<nums.size(); i++) {
+            if(nums[i] == target) {
+                index = i;
+                break;
+            }
         }
-        return -1;
+        return index;
     }
 };
