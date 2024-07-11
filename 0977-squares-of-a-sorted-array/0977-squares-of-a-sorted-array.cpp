33e8e6b04e523795f48890f8882ee2588b13c3fe
Melanie Chiu <76036950+Melaniecode@users.noreply.github.com>
2024-07-11 17:05:52 +0800

    Time: 17 ms (86.18%), Space: 28.6 MB (59.76%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,19 +1,39 @@
-// O(n + nlog n)
+// i指向起始位置，j指向终止位置
+// time O(n), space O(n)
 class Solution {
-public :
+public : 
     vector<int> sortedSquares(vector<int>& nums) {
-        int n = nums.size();
-        for(int i = 0; i < n; i++) {
-            nums[i] *= nums[i];
+        int n = nums.size()-1;
+        vector<int> result(nums.size(), 0); // 建立一個新vector，size和原本一樣，值都先填成0
+        for(int i = 0, j = nums.size() - 1; i <= j;) {
+            if(nums[i]*nums[i] < nums[j]*nums[j]) {
+                result[n--] = nums[j]*nums[j];
+                j--;
+             }   
+            else {
+                result[n--] = nums[i]*nums[i];
+                i++;
+            }
         }
-        
-        // quick sort O(nlogn)
-        sort(nums.begin(), nums.end());
-        return nums;
+        return result;
     }
 };
+// //O(n + nlog n)
+// class Solution {
+// public :
+//     vector<int> sortedSquares(vector<int>& nums) {
+//         int n = nums.size();
+//         for(int i = 0; i < n; i++) {
+//             nums[i] *= nums[i];
+//         }
+        
+//         // quick sort O(nlogn)
+//         sort(nums.begin(), nums.end());
+//         return nums;
+//     }
+// };
 
-// bubble sort
+// //bubble sort
 // class Solution {
 // public:
 //     vector<int> sortedSquares(vector<int>& nums) {
