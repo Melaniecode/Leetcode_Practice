533648dc296b6647d7499e66ba4672194c9e453c
Melanie Chiu <76036950+Melaniecode@users.noreply.github.com>
2024-07-11 16:18:16 +0800

    Time: 1006 ms (5.01%), Space: 28.6 MB (59.76%) - LeetHub

---
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -0,0 +1,21 @@
+// bubble sort
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n = nums.size();
+        
+        for(int i = 0; i < n; i++) {
+            nums[i] = pow(nums[i], 2);
+        }
+        for(int i = 0; i < n - 1; i++) {
+            for(int j = 0; j < n - 1 - i; j++) {
+                if(nums[j] > nums[j + 1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
+        return nums;
+    }
+};
