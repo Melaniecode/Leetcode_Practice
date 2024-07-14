368442383fcfcf7b82cdf034daee79822dd3d004
Melanie Chiu <76036950+Melaniecode@users.noreply.github.com>
2024-07-14 17:11:50 +0800

    Time: 23 ms (9.56%), Space: 20.1 MB (7.6%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
@@ -8,22 +8,16 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
-// 直接使用原來的來刪除。
+ 
+//設置一个虛擬節點再做刪除 O(n)
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-        // 頭結點
-        while (head != NULL && head->val == val) { 
-            ListNode* tmp = head;
-            head = head->next;
-            delete tmp;
-        }
-
-        // 非頭結點
-        ListNode* cur = head;
-        while (cur != NULL && cur->next!= NULL) {
-            if (cur->next->val == val) {
+        ListNode* dummyHead = new ListNode(0); //虛擬節點
+        dummyHead->next = head;
+        ListNode* cur = dummyHead;
+        while (cur->next != NULL) {
+            if(cur->next->val == val) {
                 ListNode* tmp = cur->next;
                 cur->next = cur->next->next;
                 delete tmp;
@@ -31,9 +25,36 @@ public:
                 cur = cur->next;
             }
         }
+        head = dummyHead->next;
+        delete dummyHead;
         return head;
     }
 };
+// // 直接使用原來的來刪除 O(n)
+// class Solution {
+// public:
+//     ListNode* removeElements(ListNode* head, int val) {
+//         // 頭結點
+//         while (head != NULL && head->val == val) { 
+//             ListNode* tmp = head;
+//             head = head->next;
+//             delete tmp;
+//         }
+
+//         // 非頭結點
+//         ListNode* cur = head;
+//         while (cur != NULL && cur->next!= NULL) {
+//             if (cur->next->val == val) {
+//                 ListNode* tmp = cur->next;
+//                 cur->next = cur->next->next;
+//                 delete tmp;
+//             } else {
+//                 cur = cur->next;
+//             }
+//         }
+//         return head;
+//     }
+// };
 
 // fast：找不含val的新值
 // slow：放新值，並update位置
