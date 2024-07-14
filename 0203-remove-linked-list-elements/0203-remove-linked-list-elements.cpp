3b0243864729cb0d7ec4ae890571f06361a05983
Melanie Chiu <76036950+Melaniecode@users.noreply.github.com>
2024-07-14 17:32:25 +0800

    Time: 25 ms (9.56%), Space: 20.2 MB (7.6%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
@@ -9,11 +9,11 @@
  * };
  */
 
-//設置一个虛擬節點再做刪除 O(n)
+//設置一個虛擬節點再做刪除 O(n)
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-        ListNode* dummyHead = new ListNode(0); //虛擬節點，這樣就不會出現NULL
+        ListNode* dummyHead = new ListNode(0); //虛擬節點
         dummyHead->next = head;
         ListNode* cur = dummyHead;
         while (cur->next != NULL) {
@@ -30,6 +30,7 @@ public:
         return head;
     }
 };
+
 // // 直接使用原來的來刪除 O(n)
 // class Solution {
 // public:
