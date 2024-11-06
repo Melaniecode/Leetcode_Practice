b44768f9019a98ee660f045df7860f429b8b2b6c
Melanie Chiu <76036950+Melaniecode@users.noreply.github.com>
2024-11-07 07:54:59 +0800

    Time: 3 ms (53.98%), Space: 18 MB (11.31%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,6 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numDict = dict()
         for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+            if target - nums[i] in numDict:
+                return numDict[target - nums[i]], i
+            numDict[nums[i]] = i
+
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         for i in range(len(nums)):
+#             for j in range(i+1, len(nums)):
+#                 if nums[i] + nums[j] == target:
+#                     return [i, j]
