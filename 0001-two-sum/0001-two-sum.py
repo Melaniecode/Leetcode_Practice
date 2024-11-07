0e1e39a2b057441162ee3731b8b9d9bee528fee9
Melanie Chiu <76036950+Melaniecode@users.noreply.github.com>
2024-11-07 08:10:33 +0800

    Time: 4 ms (43.89%), Space: 18 MB (11.31%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -4,7 +4,7 @@ class Solution:
         for i in range(len(nums)):
             if target - nums[i] in numDict:
                 return numDict[target - nums[i]], i
-            numDict[nums[i]] = i
+            numDict[nums[i]] = i # 將當前數字 nums[i] 作為鍵（key），並將其索引 i 作為值（value）存入字典 numDict
 
 # class Solution:
 #     def twoSum(self, nums: List[int], target: int) -> List[int]:
