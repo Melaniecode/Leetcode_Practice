fb1b0a61796372d8cd0baf339253f26b162b6ced
Melanie Chiu <76036950+Melaniecode@users.noreply.github.com>
2025-02-21 17:06:09 +0800

    Time: 0 ms (100%), Space: 17.8 MB (48.55%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -7,5 +7,5 @@ class Solution:
         dp[1] = 1
 
         for i in range(2, n+1):
-            dp[i] = dp[i-1] + dp[i-2]
+            dp[i] = dp[i-1] + dp[i-2] # 每次只能爬1或2階，因此第i階層會是第i-1階上排1層或是第i-2階向上爬2層
         return dp[n]
