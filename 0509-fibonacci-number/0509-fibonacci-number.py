ea4053cdacb1b7269b5960249e7c2b797a7aada8
Melanie Chiu <76036950+Melaniecode@users.noreply.github.com>
2025-02-21 16:26:30 +0800

    Time: 41 ms (53.03%), Space: 17.6 MB (88.99%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,3 +1,4 @@
+# 時間複雜度：O(n), 空間複雜度O(n)
 class Solution:
     def fib(self, n:int) -> int:
         if n == 0:
@@ -5,11 +6,12 @@ class Solution:
         dp = [0] * (n+1)
         dp[0] = 0
         dp[1] = 1
-        
+
         for i in range(2, n+1):
             dp[i] = dp[i-1]+dp[i-2]
         return dp[n]
 
+# 時間複雜度：O(2ⁿ), 空間複雜度O(n)
 # class Solution:
 #     def fib(self, n: int) -> int:
 #         if n == 1 or n == 0:
