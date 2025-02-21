672ee7d8036fe2121f5054c9fad51535b4a6a3fd
Melanie Chiu <76036950+Melaniecode@users.noreply.github.com>
2025-02-21 16:12:51 +0800

    Time: 38 ms (67.16%), Space: 17.7 MB (63.8%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,6 +1,17 @@
 class Solution:
-    def fib(self, n: int) -> int:
-        if n == 1 or n == 0:
+    def fib(self, n:int) -> int:
+        if n == 0 or n==1:
             return n
-        else:
-            return self.fib(n-1) + self.fib(n-2)
+        dp = [0] * (n+1)
+        dp[0] = 0
+        dp[1] = 1
+        for i in range(2, n+1):
+            dp[i] = dp[i-1]+dp[i-2]
+        return dp[n]
+
+# class Solution:
+#     def fib(self, n: int) -> int:
+#         if n == 1 or n == 0:
+#             return n
+#         else:
+#             return self.fib(n-1) + self.fib(n-2)
